4552b63b7997e753bcf51d17883da1172f2383d8
katicode <katimattila@outlook.com>
2019-09-08 16:25:54 +0300

    bingo coupon generating done

--- BingoApp/BingoApp/Program.cs
+++ BingoApp/BingoApp/Program.cs
@@ -21,93 +21,57 @@ namespace BingoApp
 
                 for (int n = 0; n < 5; n++)// rivien lukumäärä = 5
                 {
+                    // arvotaan numerot sarake kerrallaan (p = rivi, n = sarake)
+
                     if (n == 0) // nollasarakkeen eli ensimmäisen = B-sarakkeen käsittely
                     {
-                        //luodaan apulista mahdollisten tuplanumeroiden tarkistamista varten
-                        var apulista = new List<int>();
-                        // laskuri-muuttuja laskee, että saamme 5 numeroa
-                        int laskuri = 0;
-                        // arvotaan numeroita niin kauan kunnes saadaan yksilölliset arvot
-                        bool onkoJo = true;
-
-                            while (onkoJo == true)
-                            {
-                                // arvotaan numerot, sarake kerrallaan (p = rivi, n = sarake)
-                                int numero = ArvoNumero(1, 15);
-
-                                // etsitään onko juuri arvottu numero jo listalla
-                                bool tulos = apulista.Exists(x => x == numero);
-                                Console.WriteLine(tulos);
-                                if (tulos == false)
-                                {
-                                    if (laskuri < 5)
-                                    {
-                                        //lisätään arvottu numero apulistalle, jotta sitä ei laiteta kupongille uudelleen
-                                        apulista.Add(numero);
-
-                                        foreach (int c in apulista)
-                                        {
-                                            Console.WriteLine(c);
-                                        }
-
-                                        laskuri = laskuri + 1;
-                                    }
-                                    else
-                                    {
-                                        onkoJo = false;
-                                    }
-                                }
-                                else
-                                {
-                                    continue;
-                                }
-
-
-                            }
-
+                        List <int> apulista = SarakkeenNumeroidenLuonti(1, 15);
+                        
+                        for (int p = 0; p < 5; p++)
+                        {
+                            // kun 5 numeroa arvottu, lisätään ne bingolapulle
+                            bingolappu[p, n] = apulista[p];
+                        }
+                    }
+                    if (n == 1)
+                    {
+                        List<int> apulista = SarakkeenNumeroidenLuonti(16, 30);
 
                         for (int p = 0; p < 5; p++)
                         {
                             // kun 5 numeroa arvottu, lisätään ne bingolapulle
-
-                            Console.WriteLine(apulista[p]);
                             bingolappu[p, n] = apulista[p];
-
-                            
                         }
-                        
 
-                    }
-                    if (n == 1)
-                    {
-                        for (int p = 0; p < 5; p++) 
-                        {
-                            // arvotaan numerot sarake kerrallaan (p = rivi, n = sarake)
-                            bingolappu[p, n] = ArvoNumero(16, 30);
-                        }
                     }
                     if (n == 2)
                     {
-                        for (int p = 0; p < 5; p++) 
+                        List<int> apulista = SarakkeenNumeroidenLuonti(31, 45);
+
+                        for (int p = 0; p < 5; p++)
                         {
-                            // arvotaan numerot sarake kerrallaan (p = rivi, n = sarake)
-                            bingolappu[p, n] = ArvoNumero(31, 45);
+                            // kun 5 numeroa arvottu, lisätään ne bingolapulle
+                            bingolappu[p, n] = apulista[p];
                         }
                     }
                     if (n == 3)
                     {
-                        for (int p = 0; p < 5; p++) 
+                        List<int> apulista = SarakkeenNumeroidenLuonti(46, 60);
+
+                        for (int p = 0; p < 5; p++)
                         {
-                            // arvotaan numerot sarake kerrallaan (p = rivi, n = sarake)
-                            bingolappu[p, n] = ArvoNumero(46, 60);
+                            // kun 5 numeroa arvottu, lisätään ne bingolapulle
+                            bingolappu[p, n] = apulista[p];
                         }
                     }
                     if (n == 4)
                     {
-                        for (int p = 0; p < 5; p++) 
+                        List<int> apulista = SarakkeenNumeroidenLuonti(61, 75);
+
+                        for (int p = 0; p < 5; p++)
                         {
-                            // arvotaan numerot sarake kerrallaan (p = rivi, n = sarake)
-                            bingolappu[p, n] = ArvoNumero(61, 75);
+                            // kun 5 numeroa arvottu, lisätään ne bingolapulle
+                            bingolappu[p, n] = apulista[p];
                         }
                     }
 
@@ -147,6 +111,57 @@ namespace BingoApp
             int JokuNumero = Random.Next(min, max);
             return JokuNumero;
         }
-        
+
+        /* Tämä aliohjelma generoi 5 yksilöllistä numeroa tietyltä väliltä (lähetetty muuttujat min ja max)
+         * Kun numerot on luotu, ne lisätään apulista-muuttujaan joka palautetaan pääohjelmalle (Main)
+         */
+        public static List<int> SarakkeenNumeroidenLuonti(int min, int max)
+        {
+            //luodaan apulista mahdollisten tuplanumeroiden tarkistamista varten
+            var apulista = new List<int>();
+            // laskuri-muuttuja laskee, että saamme 5 numeroa
+            int laskuri = 0;
+            // arvotaan numeroita niin kauan kunnes saadaan yksilölliset arvot
+            bool onkoJo = true;
+
+            while (onkoJo == true)
+            {
+                // arvotaan numerot, sarake kerrallaan (p = rivi, n = sarake)
+                int numero = ArvoNumero(min, max);
+
+                // etsitään onko juuri arvottu numero jo listalla
+                bool tulos = apulista.Exists(x => x == numero);
+                //Console.WriteLine(tulos);
+                if (tulos == false)
+                {
+                    if (laskuri < 5)
+                    {
+                        //lisätään arvottu numero apulistalle, jotta sitä ei laiteta kupongille uudelleen
+                        apulista.Add(numero);
+
+                        /*foreach (int c in apulista)
+                        {
+                            Console.WriteLine(c);
+                        }*/
+
+                        laskuri = laskuri + 1;
+                    }
+                    else
+                    {
+                        onkoJo = false;
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+            return apulista;
+
+
+
+        }
+
     }
 }
